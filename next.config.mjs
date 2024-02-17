5b88ed76ddb784a7f75f90f77fab1b59d0a12e1f
rainyPoseidon21 <muhaimiemazlah0116346@gmail.com>
2024-02-18 02:37:02 +0800

    Wrap tailwindconfig with uploadThing fx

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "uploadthing.com",
+      "utfs.ui",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
+    ],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
